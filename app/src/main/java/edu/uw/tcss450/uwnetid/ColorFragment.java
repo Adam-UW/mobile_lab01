2a6d80c27b1fa2b2da8c1130a2fe3482db352080
Adam Shandi <abbashasan91@gmail.com>
2020-04-09 23:07:30 -0700

    done step 72 on Themes step

--- app/src/main/java/edu/uw/tcss450/uwnetid/ColorFragment.java
+++ app/src/main/java/edu/uw/tcss450/uwnetid/ColorFragment.java
@@ -2,6 +2,8 @@ package edu.uw.tcss450.uwnetid;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -32,6 +34,17 @@ public class ColorFragment extends Fragment {
         return view;
     }
 
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //Get a reference to the SafeArg object
+        ColorFragmentArgs args = ColorFragmentArgs.fromBundle(getArguments());
+
+        //Set the text color of the label . NOTE no need to cast
+        binding.textColorLabel.setTextColor(args.getColor());
+    }
 
     public void updateContent(int color){
         TextView tv = getActivity().findViewById(R.id.text_color_label);
