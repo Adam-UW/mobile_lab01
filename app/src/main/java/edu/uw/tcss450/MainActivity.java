fd5eb428bdd6dd4abc1c2026b4734477fc03e18c
Adam Shandi <abbashasan91@gmail.com>
2020-04-20 12:02:00 -0700

    step 133

--- app/src/main/java/edu/uw/tcss450/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/MainActivity.java
@@ -1,32 +1,14 @@
 package edu.uw.tcss450;
 
-import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-import java.util.List;
-
-import edu.uw.tcss450.databinding.ActivityMainBinding;
-
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
-
-//
-//    @Override
-//    public void onBackPressed() {
-//        super.onBackPressed();
-//        finish();
-//    }
 }
